7b7eea08569033f909c34c711e00b170cbc1fe76
danichagas <chagasdaniel788@gmail.com>
2025-07-24 16:14:09 -0300

    feat: catching all users

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -29,4 +29,20 @@ router.post('/', async (req, res) => {
   }
 })
 
+router.get('/', async (req, res) => {
+
+  try {
+
+    const users = await User.find()
+
+    res.status(200).json(users)
+
+  } catch(error) {
+
+    res.status(500).json({ error: error })
+
+  }
+
+})
+
 module.exports = router
