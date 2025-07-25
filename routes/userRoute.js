cdf9fc28f28ba4268d65f98c0e774cb1452e95bb
danichagas <chagasdaniel788@gmail.com>
2025-07-25 00:14:48 -0300

    feat: creating a users update route

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -62,4 +62,32 @@ router.get('/:id', async (req, res) => {
   }
 })
 
+router.patch('/:id', async (req, res) => {
+
+  const id = req.params.id
+
+  const { name, salary, approved } = req.body
+
+  const user = {
+    name,
+    salary,
+    approved,
+  }
+
+  try {
+
+    const updatedUser = await User.updateOne({ _id: id}, user)
+
+    if(updatedUser.matchedCount == 0) {
+      res.status(422).json({ message: 'Usuário não encontrado!' })
+      return
+    }
+
+    res.status(200).json(user)
+    
+  } catch(error) {
+    res.status(500).json({ error: error })
+  }
+})
+
 module.exports = router
